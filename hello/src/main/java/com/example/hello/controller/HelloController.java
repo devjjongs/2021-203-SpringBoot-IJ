d322b30cec7a4539594f7dfee895de4b3d7beb48
devjjongs <whdtjr5689@gmail.com>
2021-08-11 14:13:25 +0900

    modified files

--- hello/src/main/java/com/example/hello/controller/HelloController.java
+++ hello/src/main/java/com/example/hello/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.hello.controller;
 
+import com.example.hello.model.vo.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +10,8 @@ public class HelloController {
     @GetMapping("/")
     public String hello(Model model) {
         model.addAttribute("foo", "hi^^");
+        Member member = new Member();
+        member.setAttribute();
         return "index";//index.html
     }
 }
