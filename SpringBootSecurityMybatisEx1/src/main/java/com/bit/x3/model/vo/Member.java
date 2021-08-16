4e31475671f3c8092c10b0b395863d6ae8284f2f
devjjongs <whdtjr5689@gmail.com>
2021-08-16 12:03:48 +0900

    added & modified files

---
+++ SpringBootSecurityMybatisEx1/src/main/java/com/bit/x3/model/vo/Member.java
@@ -0,0 +1,11 @@
+package com.bit.x3.model.vo;
+
+
+public class Member {
+    private String userId;
+    private String userPw;
+    private String userName;
+    private String email;
+    private int age;
+    private boolean isActive;
+}
