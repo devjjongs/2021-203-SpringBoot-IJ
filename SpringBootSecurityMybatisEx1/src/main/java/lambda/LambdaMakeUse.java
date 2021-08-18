01eca820028049aa9e121141a7348b4ea8c55d03
devjjongs <whdtjr5689@gmail.com>
2021-08-18 11:24:35 +0900

    added & modified files (lambda practice)

--- SpringBootSecurityMybatisEx1/src/main/java/lambda/LambdaMakeUse.java
+++ SpringBootSecurityMybatisEx1/src/main/java/lambda/LambdaMakeUse.java
@@ -2,6 +2,23 @@ package lambda;
 
 public class LambdaMakeUse {    //  lambda를 이용한 작업 방식
     public static void main(String[] args) {
+        Operator mult = new Operator() {
 
+            @Override
+            public int calc(int a, int b) {
+                return 0;
+            }
+        };
+
+        System.out.println(mult.calc(234, 23));
+
+        Operator mult2 = /*new Operator() {
+            @Override
+            public int calc */(int a, int b) -> {
+            return a * b;
+        };
     }
+
 }
+
+
--- SpringBootSecurityMybatisEx1/src/main/java/lambda/Operator.java
+++ SpringBootSecurityMybatisEx1/src/main/java/lambda/Operator.java
@@ -1,5 +1,8 @@
 package lambda;
 
+@FunctionalInterface
 public interface Operator {
     int calc(int a, int b);
+//    @FunctionalInterface 를 사용해서 메소드가 추가되면 에러 발생
+//    void out();   //  메소드가 추가되었기 때문에 에러 발생
 }
