574c84b899cca69640b428bb61c10c387b4482b3
devjjongs <whdtjr5689@gmail.com>
2021-08-18 10:48:02 +0900

    added files (lambda practice)

---
+++ SpringBootSecurityMybatisEx1/src/main/java/lambda/Test.java
@@ -0,0 +1,11 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("");   //  동적바인딩
+    }
+}
