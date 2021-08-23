7c1fc58a18c30d40163015a5c04bfb288f4d9797
devjjongs <whdtjr5689@gmail.com>
2021-08-23 11:50:40 +0900

    added & modified file

--- SpringBootSecurityMybatisEx1/src/main/java/generics/CodeMapGenericUse.java
+++ SpringBootSecurityMybatisEx1/src/main/java/generics/CodeMapGenericUse.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class CodeMapGenericUse {
     public static void main(String[] args) {
-        Person p1 = new Person("kim1234", "kim mal ja", "서울");
-        CodeMap<Integer, Person> c1 =
-                new CodeMap<Integer, Person>(1, p1);
-
-        CodeMap<Integer, Person> c2 =
-                new CodeMap<Integer, Person>(3, new Person("kim1234", "kim mal ja", "서울"));
-
-        System.out.println(CodeMap.valueCompare(c1, c2) ? "데이터가 같음" : "데이터가 다름");
+//        Person p1 = new Person("kim1234", "kim mal ja", "서울");
+//        CodeMap<Integer, Person> c1 =
+//                new CodeMap<Integer, Person>(1, p1);
+//
+//        CodeMap<Integer, Person> c2 =
+//                new CodeMap<Integer, Person>(3, new Person("kim1234", "kim mal ja", "서울"));
+//
+//        System.out.println(CodeMap.valueCompare(c1, c2) ? "데이터가 같음" : "데이터가 다름");
     }
 }
---
+++ SpringBootSecurityMybatisEx1/src/main/java/generics/MaxMinTest.java
@@ -0,0 +1,56 @@
+package generics;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class MaxMinTest {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(4);
+        list.add(11);
+        list.add(490);
+        list.add(24);
+
+        Iterator<Integer> iterator = list.iterator();
+        int tempi = iterator.next();
+        while (iterator.hasNext()) {
+            int nextData = iterator.next();
+            if (tempi < nextData) {
+                tempi = nextData;
+                System.out.println(tempi);
+            }
+        }
+
+        System.out.println("------------iterator-------------");
+
+
+        int[] nums = {4, 11, 490, 24};
+        int temp = nums[0];
+
+        // 0번째 자리의 값 보다 큰 값을 찾는다
+        if (temp < nums[1]) // 1번이 0번보다 크다면
+            temp = nums[1]; // 11 // 1번으로 값을 바꿈
+//        else
+//            temp = temp;
+
+        if (temp < nums[2])
+            temp = nums[2]; // 424
+
+        if (temp < nums[3])
+            temp = nums[3]; // 24
+
+        System.out.println("temp = " + temp);
+        System.out.println("---------------if----------------");
+
+        temp = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (temp < nums[i])
+                temp = nums[i];
+
+            System.out.println(temp);
+        }
+        System.out.println("최종적으로 temp = " + temp);
+        System.out.println("---------------for----------------");
+    }
+}
