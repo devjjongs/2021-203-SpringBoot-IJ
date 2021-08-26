48a289b1e880691a5703bfa3be346728a517a1e5
devjjongs <whdtjr5689@gmail.com>
2021-08-26 14:31:25 +0900

    modified file

--- SpringBootJPA/src/main/java/com/bit/x4/controller/JPAExamController.java
+++ SpringBootJPA/src/main/java/com/bit/x4/controller/JPAExamController.java
@@ -1,4 +1,33 @@
 package com.bit.x4.controller;
 
+import com.bit.x4.model.dao.MemberJpaRepository;
+import com.bit.x4.model.vo_dto_entity.MemberJpa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/member")
 public class JPAExamController {
+    @Autowired
+    private MemberJpaRepository dao;
+
+
+    @RequestMapping("/hi")
+    @ResponseBody
+    public String hello() {
+
+        return "Hello";
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<MemberJpa> view() {
+        List<MemberJpa> memberData = dao.findAll();
+        return memberData;
+    }
 }
--- SpringBootJPA/src/main/java/com/bit/x4/model/vo_dto_entity/MemberJpa.java
+++ SpringBootJPA/src/main/java/com/bit/x4/model/vo_dto_entity/MemberJpa.java
@@ -1,9 +1,13 @@
 package com.bit.x4.model.vo_dto_entity;
 
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Timestamp;
 
 @Entity
 @Data
@@ -11,4 +15,13 @@ public class MemberJpa {
 
     @Id
     private int memberId;
+    private String email;
+    private String password;
+
+    @CreationTimestamp
+    private @Column(name = "cdate")
+    Timestamp cdate;
+    @UpdateTimestamp
+    private @Column(name = "udate")
+    Timestamp udate;
 }
