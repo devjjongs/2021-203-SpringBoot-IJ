68af88bedbd8bef259a4d8e5b2b226f8eafd4ab1
devjjongs <whdtjr5689@gmail.com>
2021-09-02 11:14:02 +0900

    added & modified file

---
+++ SpringBootSecurityMybatisEx1/src/main/java/bit/unit/Calculator.java
@@ -0,0 +1,19 @@
+package bit.unit;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+	public int sub(int num1, int num2) {
+		return num1 - num2;
+	}
+
+	public int multi(int num1, int num2) {
+		return num1 * num2;
+	}
+
+	public int div(int num1, int num2) {
+		return num1 / num2;
+	}
+}
---
+++ SpringBootSecurityMybatisEx1/src/main/java/bit/unit/FourArithmeticOPTest.java
@@ -0,0 +1,27 @@
+package bit.unit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import bit.unit.FourArithmeticOP;
+
+public class FourArithmeticOPTest {
+    private FourArithmeticOP cal = new FourArithmeticOP();
+    private int result;
+
+    @Test
+    public void testAdd() {
+        result = cal.addition(10, 20);
+        assertEquals(30, result, 5);
+        /**
+         * assertEquals(a, b, c); a: 예상값, b:결과값, c: 오차범위
+         */
+    }
+
+    @Test
+    public void testSubtraction() {
+        result = cal.subtraction(10, 20);
+        assertEquals(30, result, 5);
+    }
+}
